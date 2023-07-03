dd81b1a86aad6f00c8de89c2ae2e4d80e12c9e31
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-07-03 18:51:27 +0530

    Added solution - LeetHub

--- Minimal Cost - GFG/minimal-cost.cpp
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -6,6 +6,22 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    int tab(vector<int>& height, int n, int k){
+        vector<int> dp(n,0);
+        
+        for(int i=1;i<n;i++){
+            int steps=INT_MAX;
+            for(int j=1;j<=k;j++){
+                if(i-j>=0){
+                    int s=abs(height[i]-height[i-j])+dp[i-j];
+                    steps=min(steps,s);
+                }
+            }
+            dp[i]=steps;
+        }
+        return dp[n-1];
+    }
+    
     int mem(vector<int>& height, int i, int k, vector<int>&dp){
         if(i==0){
             return 0;
@@ -22,6 +38,8 @@ class Solution {
         }
         return dp[i]=steps;
     }
+    
+    
     int helper(vector<int>& height, int i, int k){
         if(i==0){
             return 0;
@@ -39,8 +57,10 @@ class Solution {
         // Code here
         // return helper(height, n-1, k);
         
-        vector<int> dp(n,-1);
-        return mem(height, n-1, k, dp);
+        // vector<int> dp(n,-1);
+        // return mem(height, n-1, k, dp);
+        
+        return tab(height, n, k);
     }
 };
 
