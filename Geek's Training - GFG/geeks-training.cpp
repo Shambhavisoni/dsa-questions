873bf657b75f98f36815f9d6b6d4d41a732cad37
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-07-04 19:33:31 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -5,27 +5,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // int tab(vector<vector<int>>& points, int day){
-    //     vector<vector<int>> dp(n, vector<int>(4,0));
-    //     int maxi=0;
-    //     for(int i=0;i<3;i++){
-    //         maxi=max(maxi,points[0][i]);
-    //         dp[0][i]=maxi;
-    //     }
+    int tab(vector<vector<int>>& points, int n){
+        vector<vector<int>> dp(n, vector<int>(4,0));
+        dp[0][0]=max(points[0][1],points[0][2]);
+        dp[0][1]=max(points[0][0],points[0][2]);
+        dp[0][2]=max(points[0][0],points[0][1]);
+        dp[0][3]=max(points[0][0],max(points[0][1], points[0][2]));
 
-    //     for(int day=1;day<=3;day++){
-    //         int maxi=0;
-    //         for(int i=0;i<3;i++){
-    //             if(i!=prev_act){
-    //                 int act=points[day][i]+dp[day-1][i];
-    //                 maxi=max(maxi,act);
-    //             }
-    //         }
-    //         dp[day][prev_act]=maxi;
-    //     }
-    //     return dp[n-1][3];
-        
-    // }
+        for(int day=1;day<n;day++){
+            for(int prev_act=0;prev_act<4;prev_act++){
+                int maxi=0;
+                for(int i=0;i<3;i++){
+                    if(i!=prev_act){
+                        int act=points[day][i]+dp[day-1][i];
+                        maxi=max(maxi,act);
+                    }
+                }
+                dp[day][prev_act]=maxi;
+            }
+        }
+        return dp[n-1][3];
+    }
     int mem(vector<vector<int>>& points, int day, int prev_act, vector<vector<int>>& dp){
         if(day==0){
             int maxi=0;
@@ -72,8 +72,10 @@ class Solution {
         // Code here
         // return recur(points, n-1, n);
         
-        vector<vector<int>> dp(n, vector<int>(4,-1));
-        return mem(points, n-1, 3, dp);
+        // vector<vector<int>> dp(n, vector<int>(4,-1));
+        // return mem(points, n-1, 3, dp);
+        
+        return tab(points, n);
     }
 };
 
