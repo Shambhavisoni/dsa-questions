d6464f6aa8cf243a2ef7bdf6dd532365dfcc1442
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-07-18 11:56:30 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,6 +9,31 @@ using namespace std;
 
 class Solution{
 public:
+    int tab(int N, int arr[]){
+        vector<vector<int>> dp(N, vector<int>(N, 0));
+        
+        // for(int i=0;i<N;i++){
+        //     dp[i][i]=0;
+        // }
+        
+        for(int i=N-1;i>=1;i--){
+            for(int j=1;j<N;j++){
+                if(i==j){
+                    dp[i][i]=0;
+                }
+                else{
+                    int mini=1e9;
+                    for(int k=i;k<j;k++){
+                        int steps=arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                        mini=min(steps, mini);
+                    }
+                    dp[i][j]=mini;
+                }
+                
+            }
+        }
+        return dp[1][N-1];
+    }
     int mem(int N, int arr[], int i, int j, vector<vector<int>>& dp){
         if(i==j){
             return 0;
@@ -39,8 +64,10 @@ public:
         // code here
         // return recur(N, arr, 1, N-1);
         
-        vector<vector<int>> dp(N, vector<int>(N, -1));
-        return mem(N, arr, 1, N-1, dp);
+        // vector<vector<int>> dp(N, vector<int>(N, -1));
+        // return mem(N, arr, 1, N-1, dp);
+        
+        return tab(N, arr);
     }
 };
 
