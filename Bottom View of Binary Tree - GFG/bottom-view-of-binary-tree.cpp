807c7f8f35f67c36ac48d87028d2173bd880f78b
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-08-03 17:34:44 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
@@ -97,28 +97,36 @@ class Solution {
   public:
     vector <int> bottomView(Node *root) {
         // Your Code Here
-        vector<int> ans;
-        if(root==NULL) return ans;
-        // node,horizontal distance
+        if(root==NULL){
+            return {};
+        }
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
         map<int,int> mp;
-        queue<pair<Node*,int>> q;
-        q.push({root,0});
+        
+        
         while(!q.empty()){
             Node* node=q.front().first;
             int hd=q.front().second;
-            
             q.pop();
+            
             mp[hd]=node->data;
+            
+            
             if(node->left){
-                q.push({node->left,hd-1});
+                q.push({node->left, hd-1});
             }
+            
             if(node->right){
-                q.push({node->right,hd+1});
+                q.push({node->right, hd+1});
             }
         }
+        
+        vector<int> ans;
         for(auto it:mp){
             ans.push_back(it.second);
         }
+        
         return ans;
     }
 };
