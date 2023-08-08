5fa9007a12f75822aa2402d81e4de8b231d28bfd
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-08-08 12:51:49 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -14,33 +14,55 @@ class Solution {
     */
     vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
         // Code here
-        vector<int> dist(V, 1e8);
-        dist[S]=0;
-        
-        
-        for(int i=1;i<V;i++){
-            for(auto it:edges){
-                int u=it[0];
-                int v=it[1];
-                int wt=it[2];
+        // vector<int> dist(V, 1e8);
+        // dist[S]=0;
+        // for(int i=0;i<V-1;i++){
+        //     for(auto it:edges){
+        //         int u=it[0];
+        //         int v=it[1];
+        //         int wt=it[2];
                 
-                if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
-                    dist[v]=dist[u]+wt;
-                }
-            }
-        }
-        
-        for(auto it:edges){
-            int u=it[0];
-            int v=it[1];
-            int wt=it[2];
+        //         if(dist[u]!=1e8 && dist[v]>dist[u]+wt){
+        //             dist[v]=dist[v]+wt;
+        //         }
+        //     }
             
-            if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
-                return {-1};
-            }
-        }
+        // }
         
-        return dist;
+        // for(auto it:edges){
+        //     int u=it[0];
+        //     int v=it[1];
+        //     int wt=it[2];
+            
+        //     if(dist[u]!=1e8 && dist[v]>dist[u]+wt){
+        //         return {-1};
+        //     }
+        // }
+        
+        // return dist;
+        
+        vector<int> dist(V, 1e8);
+		dist[S] = 0;
+		for (int i = 0; i < V - 1; i++) {
+			for (auto it : edges) {
+				int u = it[0];
+				int v = it[1];
+				int wt = it[2];
+				if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+					dist[v] = dist[u] + wt;
+				}
+			}
+		}
+		// Nth relaxation to check negative cycle
+		for (auto it : edges) {
+			int u = it[0];
+			int v = it[1];
+			int wt = it[2];
+			if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+				return { -1};
+			}
+		}
+		return dist;
     }
 };
 
