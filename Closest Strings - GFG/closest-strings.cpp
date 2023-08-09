bca3a548f496f85415797cf5a84996f702132f19
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-08-09 11:09:31 +0530

    Added solution - LeetHub

--- Closest Strings - GFG/closest-strings.cpp
+++ Closest Strings - GFG/closest-strings.cpp
@@ -11,47 +11,25 @@ public:
 	int shortestDistance(vector<string> &s, string word1, string word2)
 	{
 		// Your code goes here
-		int n=s.size();
-		map<string, vector<int>> mp;
+		int n=s.size(), mini=INT_MAX, idx1=-1, idx2=-1;
+		
 		for(int i=0;i<n;i++){
 		    if(s[i]==word1){
-		        mp[s[i]].push_back(i);
+		        idx1=i;
 		    }
 		    if(s[i]==word2){
-		        mp[s[i]].push_back(i);
+		        idx2=i;
+		    }
+		    
+		    if(idx1!=-1 && idx2!=-1){
+		        int diff=abs(idx1-idx2);
+		        mini=min(mini, diff);
 		    }
 		}
-		int mini=INT_MAX;
-		for(auto it:mp[word1]){
-		    for(auto a:mp[word2]){
-		        int diff=abs(it-a);
-		        mini=min(mini,diff);
-		    }
-		}
+		
 		return mini;
 	}
 };
-
-// 		int i=0, j=0, n=s.size();
-// 		while(i<n && j<n && s[i]!=word1){
-// 		    i++;
-// 		    j++;
-// 		}
-// 		int mini=INT_MAX;
-// 		for(int k=j;k<n;k++){
-// 		    if(s[k]==word1){
-// 		        i=k;
-// 		    }
-// 		    else if(s[k]==word2){
-// 		        mini=min(mini, k-i+1);
-// 		        i=j;
-// 		        while(i<n && j<n && s[i]!=word1){
-//         		    i++;
-//         		    j++;
-//         		}
-// 		    }
-// 		}
-// 		return mini;
 
 //{ Driver Code Starts.
 
