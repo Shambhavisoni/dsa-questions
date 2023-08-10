7377281a26ca7a5461967746194469cf90080c2f
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2023-08-10 14:06:52 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -4,75 +4,28 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
+		
+
 	public:
-	int sO(int arr[], int n){
-	    vector<int> p(n+1, 0);
-	    vector<int> c(n+1, 0);
-	    
-	    for(int curr=n-1;curr>=0;curr--){
-	        for(int prev=curr-1;prev>=-1;prev--){
-	            int take=0;
-        	    if(prev==-1 || arr[prev]<arr[curr]){
-        	        take=arr[curr]+c[curr+1];
-        	    }
-        	    int notTake=c[prev+1];
-        	    p[prev+1]=max(take, notTake);
-	        }
-	        c=p;
-	    }
-	    return p[0];
-	}
-	int tab(int arr[], int n){
-	    vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-	    
-	    for(int curr=n-1;curr>=0;curr--){
-	        for(int prev=curr-1;prev>=-1;prev--){
-	            int take=0;
-        	    if(prev==-1 || arr[prev]<arr[curr]){
-        	        take=arr[curr]+dp[curr+1][curr+1];
-        	    }
-        	    int notTake=dp[curr+1][prev+1];
-        	    dp[curr][prev+1]=max(take, notTake);
-	        }
-	    }
-	    return dp[0][0];
-	}
-	int mem(int arr[], int curr, int prev, int n, vector<vector<int>>& dp){
-	    if(curr>=n){
-	        return 0;
-	    }
-	    if(dp[curr][prev+1]!=-1){
-	        return dp[curr][prev+1];
-	    }
-	    int take=0;
-	    if(prev==-1 || arr[prev]<arr[curr]){
-	        take=arr[curr]+mem(arr, curr+1, curr, n, dp);
-	    }
-	    int notTake=mem(arr, curr+1, prev, n, dp);
-	    return dp[curr][prev+1]=max(take, notTake);
-	}
-	int recur(int arr[], int curr, int prev, int n){
-	    if(curr>=n){
-	        return 0;
-	    }
-	    int take=0;
-	    if(prev==-1 || arr[prev]<arr[curr]){
-	        take=arr[curr]+recur(arr, curr+1, curr, n);
-	    }
-	    int notTake=recur(arr, curr+1, prev, n);
-	    return max(take, notTake);
-	}
-	int maxSumIS(int arr[], int n)  
+	int maxSumIS(int nums[], int n)  
 	{  
 	    // Your code goes here
-	   // return recur(arr, 0, -1, n);
-	    
-	   // vector<vector<int>> dp(n, vector<int>(n+1, -1));
-	   // return mem(arr, 0, -1, n, dp);
-	    
-	   // return tab(arr, n);
-	    
-	    return sO(arr, n);
+	   // int n=nums.size();
+        vector<int> c(n+1, 0);
+        vector<int> p(n+1, 0);
+
+        for(int curr=n-1; curr>=0; curr--){
+            for(int prev=curr-1; prev>=-1; prev--){
+                int take=INT_MIN, notTake=INT_MIN;
+                if(prev==-1 || nums[prev]<nums[curr]){
+                    take = nums[curr] + c[curr+1];
+                }
+                notTake=c[prev+1];
+                p[prev+1]=max(take, notTake);
+            }
+            c=p;
+        }
+        return p[0];
 	}  
 };
 
